0ccd1e711d5d16841dab2ca2a83b4c7fabf8ea76
Roy <roymeplease@gmail.com>
2020-03-14 17:14:40 +0200

    instead of various ifs for the colorBtn, shortaned it to one simple line

--- dom-excercises/colorbtn.js
+++ dom-excercises/colorbtn.js
@@ -1,15 +1,8 @@
 const colorBtn = document.querySelectorAll(".colorBtn")
 
+
 colorBtn.forEach(function (element) {
   element.addEventListener("click", function() {
-    if (element.textContent === "Blue") {
-      body.style.backgroundColor = "blue";
-    } else if (element.textContent === "Red") {
-      body.style.backgroundColor = "red";
-    } else if (element.textContent === "Green") {
-      body.style.backgroundColor = "green";
-    } else {
-      body.style.backgroundColor = "black"
-    }
+    body.style.backgroundColor = element.textContent;
   });
 });
